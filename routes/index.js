2d56979dee9201fe878370c0c4cd7511f9ac4957
oreanroy <oreanroy@gmail.com>
2019-01-11 18:55:30 +0530

    rendering data from db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product')
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('shop/index', { title: 'Shopping-Cart' });
+  Product.find(function(err, docs){
+   // console.log(products.length);
+   //var productChunks = [];
+   //var chunkSize = 3;
+   //for (var i=0; i <= docs.length; i+= chunkSize){
+   //   productChunks.push(docs.slice(i, i + chunkSize));
+   //}
+    res.render('shop/index', { title: 'Shopping-Cart', products: docs });  
+  })
 });
 
 module.exports = router;
